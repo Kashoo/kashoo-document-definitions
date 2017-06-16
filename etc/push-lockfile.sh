4e33caf52777061261f344f229d8679bb7e8a36e
Joel Andrews <joel.andrews@kashoo.com>
2017-06-16 14:50:25 -0700

    CWAP-204: Ensure lockfile update is only pushed if npm supports it
    Builds on version 0.10.x of Node.js do not support npm lockfiles, so skip that step if the version is insufficient.

--- etc/push-lockfile.sh
+++ etc/push-lockfile.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -e
 
-# Have Greenkeeper update the project's npm lockfile to match the new dependency versions
+# Have Greenkeeper upload/push the project's updated npm lockfile to the git origin
 NPM_VERSION=$(npm --version)
 
 # Lockfiles are only supported on npm 5+
 IFS='.' read -ra NPM_VERSION_COMPONENTS <<< "$NPM_VERSION"
 if [[ "${NPM_VERSION_COMPONENTS[0]}" -ge 5 ]]; then
-  echo "INFO: npm version $NPM_VERSION supports lockfiles. Using Greenkeeper to update the lockfile..."
-  greenkeeper-lockfile-update
+  echo "INFO: npm version $NPM_VERSION supports lockfiles. Using Greenkeeper to push the lockfile update..."
+  greenkeeper-lockfile-upload
 else
   echo "WARN: npm version $NPM_VERSION DOES NOT support lockfiles. Skipping lockfile update..."
 fi
