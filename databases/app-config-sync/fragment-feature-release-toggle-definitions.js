e93b94048efc1155b489b9180be7beec1b46b101
ziemek trzesicki <ziemek@gmail.com>
2017-08-04 16:49:04 -0700

    CWAP-212: Simplified the feature toggle definitions so that they're easier to query.

--- databases/app-config-sync/fragment-feature-release-toggle-definitions.js
+++ databases/app-config-sync/fragment-feature-release-toggle-definitions.js
@@ -1,42 +1,47 @@
-function() {
-  var typeRegexMatchGroups = featureToggleNameRegex.exec(doc._id);
-  var PROCESSOR_ID_MATCH_GROUP = 1;
-
-  return {
-    channels: {
-      view: [ 'view-feature-release-toggle-definitions', 'view-config' ],
-      add: [ 'edit-feature-release-toggle-definitions', 'edit-config' ],
-      replace: [ 'edit-feature-release-toggle-definitions', 'edit-config' ],
-      remove: [ 'remove-feature-release-toggle-definitions', 'remove-config' ]
-    },
-    typeFilter: function(doc, oldDoc) {
-      // Example valid document IDs: "feature-release-toggle.foo-toggle", "feature-release-toggle.bar_1"
-      return new RegExp('^feature-release-toggle\.' + featureToggleNamePattern + '$').test(doc._id);
-    },
-    propertyValidators: {
-      name: {
-        type: 'string',
+{
+  channels: {
+    view: [ 'view-feature-release-toggle-definitions', 'view-config' ],
+    add: [ 'edit-feature-release-toggle-definitions', 'edit-config' ],
+    replace: [ 'edit-feature-release-toggle-definitions', 'edit-config' ],
+    remove: [ 'remove-feature-release-toggle-definitions', 'remove-config' ]
+  },
+  typeFilter: function(doc, oldDoc) {
+    return doc._id === 'featureReleaseToggleDefinitions';
+  },
+  propertyValidators: {
+    toggles: {
+      // A list of actions that are available to the recipient of the notification
+      type: 'array',
+      arrayElementsValidator: {
+        type: 'object',
         required: true,
-        mustNotBeEmpty: true,
-        immutable: true,
-        regexPattern: featureToggleNameRegex
-      },
-      description: {
-        type: 'string',
-        required: true,
-        mustNotBeEmpty: true
-      },
-      ticketId: {
-        type: 'string'
-      },
-      state: {
-        type: 'enum',
-        predefinedValues: [ 'development', 'ready', 'remove' ],
-        required: true
-      },
-      note: {
-        type: 'string'
+        propertyValidators: {
+          name: {
+            type: 'string',
+            required: true,
+            mustNotBeEmpty: true,
+            regexPattern: featureToggleNameRegex
+          },
+          description: {
+            type: 'string',
+            required: true,
+            mustNotBeEmpty: true
+          },
+          ticketId: {
+            // the ticket id related to the feature toggle, for example KBW-1234
+            type: 'string'
+          },
+          state: {
+            type: 'enum',
+            predefinedValues: [ 'development', 'ready', 'remove' ],
+            required: true
+          },
+          note: {
+            // Any extra notes about the feature toggle
+            type: 'string'
+          }
+        }
       }
     }
-  };
+  }
 }
