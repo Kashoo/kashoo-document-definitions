09656a8c022f9affaf2a02ee859d724e36ca74f6
Ziemek Trzesicki <ziemek@gmail.com>
2018-02-05 16:46:07 -0800

    CWAP-215: Update the shoebox item schema.

--- databases/business-sync/fragment-shoebox-item.js
+++ databases/business-sync/fragment-shoebox-item.js
@@ -8,36 +8,42 @@
   cannotDelete: true,
   propertyValidators: {
     type: {
-      // The type of shoebox item (ie, bank transaction, email, photo, digital receipt)
-      type: 'string',
+      // The type of shoebox item (bank transaction, uploaded document, email)
+      type: 'enum',
+      predefinedValues: [ 'bank', 'document', 'email' ],
       required: true,
       immutable: true,
       mustNotBeEmpty: true
     },
     source: {
-      // The source of the item
+      // The source of the item (yodlee, shoebox-ui...)
       type: 'string',
       required: true,
       immutable: true,
       mustNotBeEmpty: true
     },
     sourceId: {
+      // The unique id given to the item by the source
       type: 'string',
       immutable: true,
       mustNotBeEmpty: true
     },
     received: {
-      // Time at which the data was received
+      // The time at which the data was received by the shoebox
       type: 'datetime',
       required: true
     },
     data: {
-      // Document describing the data
+      // Document describing the data (this is determined by the source and isn't important to the shoebox).
+      // If this data is updated by the source, this property should be updated to the most recent value and the previous
+      // data should be moved to the previousData property.
       type: 'object',
       required: true,
       allowUnknownProperties: true
     },
     previousData: {
+      // If the data changes after it was put in the shoebox, this array stores the previous values that were originally
+      // stored as a historical reference.
       type: 'array',
       arrayElementsValidator: {
         type: 'object',
