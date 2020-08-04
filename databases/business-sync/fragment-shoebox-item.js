c6ac6a303d2628a7f69f23980fa648a35f00066f
Ziemek Trzesicki <ziemek.trzesicki@kashoo.com>
2020-08-04 09:56:12 -0700

    CWAP-1741: Add payment-deposit document types.

--- databases/business-sync/fragment-shoebox-item.js
+++ databases/business-sync/fragment-shoebox-item.js
@@ -13,7 +13,7 @@
     type: {
       // The type of shoebox item (bank transaction, uploaded document, email)
       type: 'enum',
-      predefinedValues: [ 'bank', 'document', 'email', 'manual-entry', 'payment' ],
+      predefinedValues: [ 'bank', 'document', 'email', 'manual-entry', 'payment', 'payment-deposit' ],
       required: true,
       immutable: true
     },
